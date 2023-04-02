7224686c53087383ee45d15644e412aa1f09d686
Ouze <ossama.benjemaa@gmail.com>
2023-04-02 14:46:56 +0200

    TODO-6: Add Reviews on restaurant page

--- src/components/restaurant/card.vue
+++ src/components/restaurant/card.vue
@@ -9,26 +9,32 @@ defineProps<{
 
 <template>
   <VCard>
-    <VImg height="100" cover :src="restaurant?.photos?.[0]" />
-    <VCardTitle>
+  <VImg height="100" cover :src="restaurant?.photos?.[0]" />
+  <VCardTitle>
     {{ restaurant?.name }}
   </VCardTitle>
-  <VAlert variant="tonal" type="warning" class="mx-4 w-1/2">
-    TODO: display the average rating
+    <VAlert variant="tonal" type="warning" class="mx-4 w-1/2">
+      TODO: display the average rating
       <br>
       Vuetify has a component for this. Use this one
     </VAlert>
 
     <!-- 
-          I wanted to use the v-for loop but after many tests 
-          even with the documentation I didn't succeed, so I used 
-          an external function I called averageRating
-      -->
+            I wanted to use the v-for loop but after many tests 
+            even with the documentation I didn't succeed, so I used 
+            an external function I called averageRating
+        -->
     <!-- Use half increments to give more detail to the user -->
-    <VRating half-increments readonly :model-value="averageRating(restaurant?.reviews)" />
+    <VRating 
+    half-increments 
+    readonly 
+    :model-value="averageRating(restaurant?.reviews)" 
+    
+    />
     <VCardText>
       <RestaurantLocation :location="restaurant?.location" />
-    </VCardText>
+    </VCardText> 
+
     <VCardActions class="mt-auto">
       <VBtn color="primary" variant="tonal" :to="`/restaurants/${restaurant?.id}`">
         see more
--- src/components/restaurant/review.vue
+++ src/components/restaurant/review.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+import { type Review } from '~/composables/restaurants';
+
+defineProps<{
+  reviews: Review
+}>();
+</script>
+
 <template>
   <VCard class="va-company-review" tag="li">
     <VCardText>
@@ -7,7 +15,12 @@
         • Rating using the appropriate Vuetify component
         <br>
         • Text (with carriage return)
-      </VAlert>
+      </VAlert>      
+    </VCardText>
+
+    <VCardText v-for="review of reviews" :key="review">
+      <VRating half-increments readonly :model-value="review.rating" />
+      <pre v-text="review.text" />
     </VCardText>
   </VCard>
 </template>
