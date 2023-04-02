4fc603940f242ba75fe7f145f03a36f9b3d52b3d
Ouze <ossama.benjemaa@gmail.com>
2023-04-02 11:03:56 +0200

    TODO-2: Create average function/ Add average rating on cards

--- src/components/restaurant/card.vue
+++ src/components/restaurant/card.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { type Restaurant } from '~/composables/restaurants';
+import { averageRating } from '~/composables/restaurants';
 
 defineProps<{
   restaurant: Restaurant
@@ -21,6 +22,18 @@ defineProps<{
       <br>
       Vuetify has a component for this. Use this one
     </VAlert>
+
+    <!-- 
+        I wanted to use the v-for loop but after many tests 
+        even with the documentation I didn't succeed, so I used 
+        an external function I called averageRating
+    -->
+    <!-- Use half increments to give more detail to the user -->
+    <VRating
+      half-increments
+      readonly
+      :model-value="averageRating(restaurant?.reviews)"
+    />
     <VCardText>
       <RestaurantLocation :location="restaurant?.location" />
     </VCardText>
